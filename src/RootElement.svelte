6771e7e21c1d1c4cf6679523a9fe5bb9f5d5f133
Li,Jiahao <cuteloli@tencent.com>
2024-08-08 16:56:05 +0800

    init

---
+++ src/RootElement.svelte
@@ -0,0 +1,21 @@
+<svelte:options
+  customElement={{
+    tag: 'root-custom-element',
+    props: {},
+  }}
+/>
+
+<script lang='ts'>
+  function setMyElementVal() {
+    ($host().querySelector('my-custom-element') as any).value = 'hello world'
+  }
+</script>
+
+<slot />
+<button onclick={() => setMyElementVal()}>click me</button>
+
+<style>
+  :host {
+    display: block;
+  }
+</style>
