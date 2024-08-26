e615f2e049b0abd86fa1a40bcac269b7854de106
Li,Jiahao <cuteloli@tencent.com>
2024-08-26 22:22:22 +0800

    init

---
+++ src/MyElement.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  document.addEventListener('click', (e) => {
+    console.log('document', e, e.currentTarget);
+  })
+  document.getElementById('app')?.addEventListener('click', (e) => {
+    console.log('#app', e, e.currentTarget);
+  })
+  function onclick(event: Event) {}
+</script>
+
+<svelte:options
+  customElement={{
+    tag: 'my-custom-element',
+  }}
+/>
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div {onclick}>
+  <slot />
+</div>
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import { default as MyElement } from './MyElement.svelte';
+
+export { MyElement };
